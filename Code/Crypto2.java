67b8fb9b45737ca3d7ce42213d71d637b0f25202
Jawher Dridi <dridijawher43@gmail.com>
2020-04-04 11:52:56 +0100

    Update Crypto2.java

--- Code/Crypto2.java
+++ Code/Crypto2.java
@@ -1,26 +1,29 @@
-package crypto;
-
-public class Crypto2 {
-
-    //fonction chiffrer qui parcourt les cles et fait un travail similaire que dans crypto
-    public static String chiffrer(String texte, String cle1, String cle2) {
-        StringBuffer texteChiffre = new StringBuffer();
-        texteChiffre.append(texte);
-        if (cle1==cle2){//si les deux cles sont identiques alors pas besoin de faire le parcours
-            return texte;
-        }else{
-            for (int i = 0; i < cle2.length(); i++) {
-                while (texteChiffre.indexOf(String.valueOf(cle1.charAt(i))) != -1) {
-                    int index = texteChiffre.indexOf(String.valueOf(cle1.charAt(i)));
-                    texteChiffre.replace(index, index + 1, String.valueOf(cle2.charAt(i)));
-                }
-            }
-        }
-
-        return texteChiffre.toString();
-    }
-//De meme, dechiffrer fait le meme travail mais avec des cles inverses
-    public static String dechiffrer(String texte, String cle1, String cle2) {
-        return chiffrer(texte, cle2, cle1);
-    }
-}
+package crypto; 
+ 
+public class Crypto2 { 
+ 
+    public static String chiffrer(String texte, Cle cle) { 
+        StringBuffer texteChiffre = new StringBuffer(); 
+        texteChiffre.append(texte); 
+        for (int i = 0 ; i < cle.substituant.length() ; i ++ ){ 
+            while (texteChiffre.indexOf(String.valueOf(cle.substitue.charAt(i))) != -1) { 
+                int index = texteChiffre.indexOf(String.valueOf(cle.substitue.charAt(i))); 
+                texteChiffre.replace(index, index + 1, String.valueOf(cle.substituant.charAt(i))); 
+            } 
+        } 
+        return texteChiffre.toString(); 
+    } 
+ 
+    public static String dechiffrer(String texte, Cle cle) { 
+        StringBuffer texteChiffre = new StringBuffer(); 
+        texteChiffre.append(texte); 
+        for (int i = 0 ; i < cle.substituant.length() ; i ++ ){ 
+            while (texteChiffre.indexOf(String.valueOf(cle.substituant.charAt(i))) != -1) { 
+                int index = texteChiffre.indexOf(String.valueOf(cle.substituant.charAt(i))); 
+                texteChiffre.replace(index, index + 1, String.valueOf(cle.substitue.charAt(i))); 
+            } 
+        } 
+        return texteChiffre.toString(); 
+    } 
+ 
+} 
